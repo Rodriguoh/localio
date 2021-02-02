2a526f4444d64245613dcf8a60c7054e966c775f
rodriguoh <jesone81@gmail.com>
2021-02-02 11:57:16 +0100

    composant menu

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('exemple', function () {
+    return view('pages/exemple');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
