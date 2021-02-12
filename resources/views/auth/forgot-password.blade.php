cd9f31438db99a43422ecdb5215983d6f21fc401
ag7 <alexis26999@gmail.com>
2021-02-12 11:47:40 +0100

    style for auth

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,12 +1,6 @@
 @include('layouts.app')
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
+<div class="auth-card">
+    <h1>Mots de passe oubliés ?</h1>
         <div class="mb-4 text-sm text-gray-600">
             {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
         </div>
@@ -21,17 +15,16 @@
             @csrf
 
             <!-- Email Address -->
-            <div>
+            <div class="auth-input">
                 <x-label for="email" :value="__('Email')" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
+            <div class="auth-block-button">
+                <x-button class="auth-submit">
                     {{ __('Email Password Reset Link') }}
                 </x-button>
             </div>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+    </div>
