b537df18b568e06d8b7a2113db1c1b6bbafc7a34
ag7 <alexis26999@gmail.com>
2021-02-18 15:59:16 +0100

    Add store

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\State;
 use App\Models\Store;
+use App\Models\City;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class StoreController extends Controller
 {
@@ -15,13 +20,67 @@ class StoreController extends Controller
     public function formStore($idStore = null)
     {
         $store = Store::find($idStore);
-        //dd(isset($store));
         return view('pages/account/formStore', [
-            'store' => isset($store) ? $store : new Store()
+            'store' => isset($store) ? $store : new Store(),
+            'categories' => Category::getCategoriesWithChild(),
         ]);
     }
 
-    public function postStore(Request $req) {
-        dd($req);
+    public function postStore(Request $request)
+    {
+
+        $this->validate($request, [
+            'name' => 'required',
+            'phone' => 'required|digits:10',
+            'mail' => 'required|email',
+            'SIRET' => 'required|digits:14',
+            'url' => 'url',
+            'category_id' => 'required|exists:categories,id',
+            'number' => 'required',
+            'street' => 'required',
+            'city' => 'required',
+        ]);
+
+        $store = new Store([
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'mail' => $request->mail,
+            'SIRET' => $request->SIRET,
+        ]);
+
+        $store->codeComment = Str::random(10);
+        $store->category_id = $request->category_id;
+        $store->url = $request->url;
+        $store->description = $request->editor;
+        $store->delivery = isset($request->delivery);
+        $store->conditionDelivery = $store->delivery ? $request->conditionDelivery : null;
+        $store->user_id = Auth::id();
+        $store->state_id = State::select('id')->where('label', '=', 'approved')->first()->id;
+
+        // adresse
+        $store->number = $request->number;
+        $store->street = $request->street;
+
+        if (City::where('name', '=', $request->city)->exists()) {
+            $store->city_INSEE = City::where('name', '=', $request->city)->first()->INSEE;
+        } else {
+            $city = new City([
+                'name' => $request->city,
+                'ZIPcode' => $request->ZIPCode,
+                'INSEE' => $request->INSEE,
+            ]);
+
+            $city->save();
+            $store->city_INSEE = $city->INSEE;
+        }
+
+
+        //localisation provisoire
+        $store->lng = $request->lng;
+        $store->lat = $request->lat;
+
+        $store->save();
+
+        return redirect()->route('homeAccount');
     }
 }
--- resources/views/pages/account/formStore.blade.php
+++ resources/views/pages/account/formStore.blade.php
@@ -4,24 +4,47 @@
 
 <!-- Commerçant -->
 {{--  --}}
+<div class="card">
+    <h2 class="card-title">
+        Information du commerce
+      </h2>
 <form action="{{route('postStore')}}" method="POST" class="w-400 mw-full">
     @csrf
     <input type="hidden" name="id" value="{{$store->id}}">
-    <input type="hidden" name="user_id" value="{{Auth::id()}}">
     <div class="form-group">
         <label for="name" class="required">Nom du commerce</label>
+        @if($errors->has('name'))
+                <div class="invalid-feedback">
+                    {{$errors->first('name')}}
+                  </div>
+                @endif
         <input type="text" class="form-control" id="name" placeholder="Nom entier" name="name">
     </div>
     <div class="form-group">
         <label for="phone" class="required">Numéro de téléphone</label>
+        @if($errors->has('phone'))
+                <div class="invalid-feedback">
+                    {{$errors->first('phone')}}
+                  </div>
+                @endif
         <input type="text" class="form-control" id="phone" placeholder="Numéro" name="phone">
     </div>
     <div class="form-group">
         <label for="mail" class="required">Adresse Mail</label>
+        @if($errors->has('mail'))
+                <div class="invalid-feedback">
+                    {{$errors->first('mail')}}
+                  </div>
+                @endif
         <input type="text" class="form-control" id="mail" placeholder="Mail" name="mail">
     </div>
     <div class="form-group">
         <label for="SIRET" class="required">Numéro de SIRET</label>
+        @if($errors->has('SIRET'))
+                <div class="invalid-feedback">
+                    {{$errors->first('SIRET')}}
+                  </div>
+                @endif
         <input type="text" class="form-control" id="SIRET" placeholder="SIRET" name="SIRET">
     </div>
     <div class="form-group">
@@ -29,11 +52,79 @@
         <input type="text" class="form-control" id="url" placeholder="L'url" name="url">
     </div>
 
+    <div class="form-group">
+        <label for="category" class="required">Categorie</label>
+        <select class="form-control" id="category" required name="category_id">
+            <option value="" selected="selected" disabled="disabled">Choisir une catégorie</option>
+            @foreach($categories as $category)
+                <option value="{{$category->id}}">{{$category->label}}</option>
+                @foreach($category->child as $categoryChild)
+                    <option value="{{$categoryChild->id}}"> -  {{$categoryChild->label}}</option>
+                @endforeach
+            @endforeach
+        </select>
+    </div>
+
     <textarea name="editor" id="editor"></textarea>
+
+    <h3 class="card-title">
+        Localisation
+    </h3>
+
+    <div>
+        <label for="number" class="required">Adresse</label>
+        <div class="form-row row-eq-spacing">
+        <div class="col">
+            <input type="text" class="form-control" id="number" placeholder="Numéro" name="number" required="required">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control" id="street" placeholder="Rue" name="street" required="required">
+        </div>
+        </div>
+    </div>
+
+    <h4>Provisoire</h4>
+    <div class="form-group">
+        <label for="city">Ville</label>
+        <input type="text" class="form-control" id="city" placeholder="Le nom de la ville" name="city">
+    </div>
+    <div class="form-group">
+        <label for="INSEE">INSEE</label>
+        <input type="text" class="form-control" id="INSEE" placeholder="Numéro INSEE" name="INSEE">
+    </div>
+    <div class="form-group">
+        <label for="ZIPCode">Code Postal</label>
+        <input type="text" class="form-control" id="ZIPCode" placeholder="Code postal" name="ZIPCode">
+    </div>
+
+    <H3>Localisation Provisoire</H3>
+    <div class="form-group">
+        <label for="lng">Longitude</label>
+        <input type="text" class="form-control" id="lng" name="lng">
+    </div>
+    <div class="form-group">
+        <label for="lat">Latitude</label>
+        <input type="text" class="form-control" id="lat" name="lat">
+    </div>
+
+    <h3 class="card-title">
+        Livraison
+        </h3>
+    <div class="custom-switch">
+        <input type="checkbox" id="delivery" value="true" name="delivery">
+        <label for="delivery">Propose la livraison</label>
+    </div>
+
+    <div class="form-group">
+        <label for="conditionDelivery">Condition de livraison</label>
+        <textarea class="form-control" id="conditionDelivery" name="conditionDelivery" placeholder="Condition de livraison"></textarea>
+    </div>
+
+
 
 
     <input class="btn btn-success" type="submit" value="Valider">
-
+</div>
     <script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
     <script>
         var editor = CKEDITOR.replace( 'editor', {
