7631a992c5ed095bbf08bea8e5b4b412731c5712
ag7 <alexis26999@gmail.com>
2021-03-01 18:29:17 +0100

    add picture for store + api to get city and coord's

--- resources/views/components/store-form.blade.php
+++ resources/views/components/store-form.blade.php
@@ -1,13 +1,13 @@
-<form action="{{route('postStore')}}" method="POST" class="mw-full">
+<form name="form" id="form" action="{{route('postStore')}}" method="POST" class="mw-full" enctype="multipart/form-data">
     @csrf
     <input type="hidden" name="id" value="{{$store->id}}">
     <div class="form-group w-400 mw-full">
         <label for="name" class="required">Nom du commerce</label>
         @if($errors->has('name'))
-                <div class="invalid-feedback">
-                    {{$errors->first('name')}}
-                </div>
-                @endif
+            <div class="invalid-feedback">
+                Le nom de commerce est obligatoire.
+            </div>
+        @endif
         <input type="text" class="form-control" id="name" placeholder="Nom entier" name="name"
         value="{{old('name', $store->name)}}">
     </div>
@@ -15,38 +15,53 @@
     <div class="form-group w-400 mw-full">
         <label for="short_description" class="required">Description courte</label>
         @if($errors->has('short_description'))
-                <div class="invalid-feedback">
-                    {{$errors->first('short_description')}}
-                </div>
-                @endif
+            <div class="invalid-feedback">
+                La description courte est obligatoire.
+            </div>
+        @endif
         <textarea class="form-control" id="short_description" name="short_description">{{old('short_description', $store->short_description)}}</textarea>
     </div>
+
+
+    <div class="form-group">
+        <label class="required">Une photo pour mettre en avant votre commerce</label>
+        @if($errors->has('photo'))
+            <div class="invalid-feedback">
+                La photo est obligatoire.
+            </div>
+        @endif
+        <div class="custom-file">
+            <input type="file" id="photo" name="photo" accept="image/*">
+            <label for="photo">Choisir une photo</label>
+        </div>
+    </div>
+
 
     <div class="form-group w-400 mw-full">
         <label for="phone" class="required">Numéro de téléphone</label>
         @if($errors->has('phone'))
-                <div class="invalid-feedback">
-                    {{$errors->first('phone')}}
-                </div>
-                @endif
+            <div class="invalid-feedback">
+                Le numéro de téléphone de votre commerce est obligatoire.
+            </div>
+        @endif
         <input type="text" class="form-control" id="phone" placeholder="Numéro" name="phone" value="{{old('phone', $store->phone)}}">
     </div>
     <div class="form-group w-400 mw-full">
         <label for="mail" class="required">Adresse Mail</label>
         @if($errors->has('mail'))
-                <div class="invalid-feedback">
-                    {{$errors->first('mail')}}
-                </div>
-                @endif
+            <div class="invalid-feedback">
+                L'adresse mail de votre commerce est obligatoire.{{$errors->first('mail')}}
+            </div>
+        @endif
         <input type="text" class="form-control" id="mail" placeholder="Mail" name="mail" value="{{old('mail', $store->mail)}}">
     </div>
     <div class="form-group w-400 mw-full">
         <label for="SIRET" class="required">Numéro de SIRET</label>
         @if($errors->has('SIRET'))
-                <div class="invalid-feedback">
-                    {{$errors->first('SIRET')}}
-                </div>
-                @endif
+            <div class="invalid-feedback">
+                Le numéro de SIRET est obligatoire.
+            </div>
+        @endif
         <input type="text" class="form-control" id="SIRET" placeholder="SIRET" name="SIRET" value="{{old('SIRET', $store->SIRET)}}">
     </div>
     <div class="form-group w-400 mw-full">
@@ -78,6 +93,11 @@
 
     <div class="w-400 mw-full">
         <label for="number" class="required">Adresse</label>
+        @if($errors->has('number') || $errors->has('street'))
+            <div class="invalid-feedback">
+                Le numéro et le nom de la rue sont obligatoire.
+            </div>
+        @endif
         <div class="form-row row-eq-spacing">
             <div class="col">
                 <input type="text" class="form-control" id="number" placeholder="Numéro" name="number" required="required" value="{{old('number', $store->number)}}">
@@ -88,29 +108,30 @@
         </div>
     </div>
 
-    <h4>Provisoire</h4>
-    <div class="form-group w-400 mw-full">
-        <label for="city">Ville</label>
-        <input type="text" class="form-control" id="city" placeholder="Le nom de la ville" name="city" value="{{old('city', $store->city->name ?? '')}}">
-    </div>
-    <div class="form-group w-400 mw-full">
-        <label for="INSEE">INSEE</label>
-        <input type="text" class="form-control" id="INSEE" placeholder="Numéro INSEE" name="INSEE" value="{{old('INSEE', $store->city_INSEE)}}">
-    </div>
-    <div class="form-group w-400 mw-full">
-        <label for="ZIPCode">Code Postal</label>
-        <input type="text" class="form-control" id="ZIPCode" placeholder="Code postal" name="ZIPCode" value="{{old('ZIPCode', $store->city->ZIPcode ?? '')}}">
+    <div class="form-group w-400 mw-full position-relative">
+        <label for="city" class="required">Ville</label>
+        @if($errors->has('INSEE'))
+            <div class="invalid-feedback">
+                {{$errors->first('INSEE')}}
+            </div>
+        @endif
+        <input type="text" class="form-control" id="city" placeholder="Le nom de la ville" name="city" value="{{old('city', $store->city->name ?? '')}}" autocomplete="new-password">
+        <ul class="position-absolute d-none auto-comp z-10 text-dark-lm text-light-dm bg-dark-light-dm bg-light-lm w-full" id="autocomplete">
+        </ul>
+        <p>Cliquez sur votre ville quand elle apparraitera dans la liste.</p>
+        @if($errors->has('lat'))
+            <div class="invalid-feedback">
+                Impossible de localiser votre adresse, si cette erreur persiste, contacté un administrateur.
+            </div>
+        @endif
     </div>
 
-    <H3>Localisation Provisoire</H3>
-    <div class="form-group w-400 mw-full">
-        <label for="lng">Longitude</label>
-        <input type="text" class="form-control" id="lng" name="lng" value="{{old('lng', $store->lng)}}">
-    </div>
-    <div class="form-group w-400 mw-full">
-        <label for="lat">Latitude</label>
-        <input type="text" class="form-control" id="lat" name="lat" value="{{old('lat', $store->lat)}}">
-    </div>
+    <input type="hidden" id="INSEE" name="INSEE" value="{{old('INSEE', $store->city_INSEE)}}">
+    <input type="hidden" id="ZIPCode" name="ZIPCode" value="{{old('ZIPCode', $store->city->ZIPcode)}}">
+
+    <input type="hidden" id="lng" name="lng" value="{{old('lng', $store->lng)}}">
+
+    <input type="hidden" id="lat" name="lat" value="{{old('lat', $store->lat)}}">
 
     <h3 class="card-title">
         Livraison
@@ -128,8 +149,17 @@
         <textarea class="form-control" id="conditionDelivery" name="conditionDelivery" placeholder="Condition de livraison">{{old('conditionDelivery', $store->conditionDelivery)}}</textarea>
     </div>
 
-    <input class="btn btn-success" type="submit" value="Valider">
+    <input class="btn btn-success" id="subStore" type="button" value="Valider">
 
+    <style>
+        #city:focus + .auto-comp {
+            display: block !important;
+        }
+
+        .auto-comp:hover {
+            display: block !important;
+        }
+    </style>
     <script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
     <script>
         var editor = CKEDITOR.replace( 'editor', {
@@ -139,5 +169,34 @@
             width:'100%',
             height:500
         } );
+    </script>
+    <script>
+        const listCity = document.getElementById('autocomplete');
+        document.getElementById('city').addEventListener('keyup', async (event) => {
+            let req = await fetch(`https://geo.api.gouv.fr/communes?nom=${event.srcElement.value}&limit=8`)
+            let rep = await req.json();
+            listCity.innerHTML = '';
+            rep.map((city) => {
+                let li = document.createElement('li');
+                li.classList.add('nav-link')
+                li.append(city.nom);
+                listCity.appendChild(li);
+                li.addEventListener('click', () => {
+                    document.getElementById('city').value = city.nom;
+                    document.getElementById('INSEE').value = city.code;
+                    document.getElementById('ZIPCode').value = city.codesPostaux;
+                })
+            });
+        });
+
+        document.getElementById('subStore').addEventListener('click', async () => {
+            let [number, street, city] = [...document.querySelectorAll(['#number', '#street', '#city'])];
+            let req = await fetch(`https://api-adresse.data.gouv.fr/search/?q=${number.value + ' ' + street.value + ' ' + city.value}&type=housenumber`);
+            let rep = await req.json();
+            let [lng, lat] = rep?.features?.[0]?.geometry?.coordinates;
+            document.getElementById('lat').value = lat;
+            document.getElementById('lng').value = lng;
+            document.form.submit();
+        });
     </script>
 </form>
