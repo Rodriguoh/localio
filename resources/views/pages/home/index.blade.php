7aafb22d4a6bcefc62114b99950b8b0b9379ef27
TheoLeao <theoleaoboudier@gmail.com>
2021-03-22 20:38:05 +0100

    show login or my account in navbar if the user as logged or not

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -36,24 +36,7 @@
 
 <body class="full-with">
     <div id="app">
-        <nav v-show="!showStore">
-            <div class="logo">
-                <img src="{{ asset('img/logo_inline-clair.svg') }}" alt="logo de click and collect">
-            </div>
-            <div v-on:click="mobileMenu" class="hamburger">
-                <div class="line"></div>
-                <div class="line"></div>
-                <div class="line"></div>
-            </div>
-            <ul class="nav-links">
-                <li><a class="linkMenu" href="{{ route('home') }}"><img
-                            src="{{ asset('img/icons/fa-home.svg') }}">Accueil</a></li>
-                <li><a class="linkMenu" href="{{ route('homeAccount') }}"><img
-                            src="{{ asset('img/icons/fa-login.svg') }}">Se connecter</a></li>
-                <li><a href="#0" class="btn-color btn-secondary btn-xs">Aide</a></li>
-            </ul>
-        </nav>
-
+        @include('components.navbar')
         <div class="margin-constraint" v-show="!showStore">
             <div class="useful-width">
                 <div class="header">
