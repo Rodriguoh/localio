cd40575dbee4c21a6a616b152d6172215765aac4
Rodriguoh <jesone81@gmail.com>
2021-03-23 18:03:12 +0100

    reset-password et refonte login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,30 +1,53 @@
 @extends('layouts.auth')
-@section('content.register')
-<h1>Inscription</h1>
+@section('content')
 
-<x-auth-validation-errors class="mb-4" :errors="$errors" />
+    <div class="select-type">
 
-<form method="POST" action="{{ route('register') }}" class="w-full">
+        <a href="{{route('login')}}">CONNEXION</a>
+
+        <a href="{{route('register')}}" class="active">INSCRIPTION</a>
+
+    </div>
+
+    <!-- Container largeur utile -->
+    <div class="login-section">
+
+
+    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+    <form method="POST" action="{{ route('register') }}" class="w-full">
     @csrf
     <!-- Email Address -->
-    <div>
-        <x-input id="email" class="form-control form-control-lg" type="email" name="email" :value="old('email')" placeholder="Email" style="margin-bottom: -1px;border-bottom-right-radius: 0;border-bottom-left-radius: 0" required />
-    </div>
-    <!-- Password -->
-    <div class="">
-        <x-input id="password" class="form-control form-control-lg" type="password" name="password" placeholder="Mot de passe" required autocomplete="new-password" style="margin-bottom: -1px;border-radius:0" />
-    </div>
-    <!-- Confirm Password -->
-    <div class="">
-        <x-input id="password_confirmation" class="form-control form-control-lg" type="password" name="password_confirmation" placeholder="Confirmation mot de passe" style="margin-bottom:-1px; border-top-left-radius:0;border-top-right-radius:0" required />
+        <div class="login-input">
+            <x-input
+                id="email" class="form-control form-control-lg" type="email" name="email" :value="old('email')" placeholder="Email" style="margin-bottom: -1px;border-bottom-right-radius: 0;border-bottom-left-radius: 0" required />
+        </div>
+        <!-- Password -->
+        <div class="login-input">
+            <x-input
+                id="password"
+                class="form-control form-control-lg"
+                type="password"
+                name="password"
+                placeholder="Mot de passe"
+                pattern=".{8,}"
+                title="Votre mot de passe doit contenir au moins 8 caractères"
+                autocomplete="new-password"
+                style="margin-bottom: -1px;border-radius:0"
+                required/>
+        </div>
+        <!-- Confirm Password -->
+        <div class="login-input">
+            <x-input
+                id="password_confirmation" class="form-control form-control-lg" type="password" name="password_confirmation" placeholder="Confirmation mot de passe" style="margin-bottom:-1px; border-top-left-radius:0;border-top-right-radius:0" required />
+            <label for="password_confirmation" style="margin: 15px 0 0 15px">Mot de passe de 8 caractères minimum</label>
+        </div>
+
+        <div class="d-flex flex-column m-10">
+            <button type="submit" class="btn btn-action btn-r8 login-btn">S'INSCRIRE</button>
+        </div>
+    </form>
     </div>
 
-    <div class="d-flex flex-column m-10">
-        <a class="" href="{{ route('login') }}">
-            Vous avez déjà un compte ?
-        </a>
-        <button type="submit" class="btn btn-primary m-10">S'inscrire</button>
-    </div>
-</form>
 
 @endsection
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,33 +1,50 @@
 @extends('layouts.auth')
 @section('content')
-<h1>Reinitialiser mot de passe</h1>
+
+
+    <div class="select-type">
+
+        <a href="{{route('login')}}">CONNEXION</a>
+
+        <a href="{{route('register')}}">INSCRIPTION</a>
+
+    </div>
+
+    <!-- Container largeur utile -->
+    <div class="login-section">
+
+        <h1>Reinitialiser mot de passe</h1>
+        <!-- Session Status -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('password.update') }}">
+        @csrf
+
+        <!-- Password Reset Token -->
+            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+            <!-- Email Address -->
+            <div class="login-input">
+                <x-input id="email" class="form-control form-control-lg" type="email" name="email" :value="old('email', $request->email)" style="margin-bottom: -1px;border-bottom-right-radius: 0;border-bottom-left-radius: 0" required disabled autofocus />
+            </div>
+
+            <!-- Password -->
+            <div class="login-input">
+                <x-input id="password" class="form-control form-control-lg" type="password" name="password" style="margin-bottom: -1px;border-radius:0" required placeholder="Nouveau mot de passe" />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="login-input">
+                <x-input id="password_confirmation" class="form-control form-control-lg mb-4" type="password" name="password_confirmation" style="margin-bottom:-1px; border-top-left-radius:0;border-top-right-radius:0" required placeholder="Confirmation mot de passe"/>
+            </div>
+
+            <div class="m-10">
+                <button type="submit" class="btn btn-action btn-r8 login-btn">REINITIALISER</button>
+            </div>
+        </form>
+
+
+    </div>
 <!-- Validation Errors -->
-<x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-<form method="POST" action="{{ route('password.update') }}">
-    @csrf
-
-    <!-- Password Reset Token -->
-    <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-    <!-- Email Address -->
-    <div>
-        <x-input id="email" class="form-control form-control-lg" type="email" name="email" :value="old('email', $request->email)" style="margin-bottom: -1px;border-bottom-right-radius: 0;border-bottom-left-radius: 0" required autofocus />
-    </div>
-
-    <!-- Password -->
-    <div>
-        <x-input id="password" class="form-control form-control-lg" type="password" name="password" style="margin-bottom: -1px;border-radius:0" required placeholder="Nouveau mot de passe" />
-    </div>
-
-    <!-- Confirm Password -->
-    <div>
-        <x-input id="password_confirmation" class="form-control form-control-lg mb-4" type="password" name="password_confirmation" style="margin-bottom:-1px; border-top-left-radius:0;border-top-right-radius:0" required placeholder="Confirmation mot de passe"/>
-    </div>
-
-    <div>
-        <button type="submit" class="btn btn-lg btn-primary btn-block mt-10">Envoyer</button>
-    </div>
-</form>
 
 @endsection
